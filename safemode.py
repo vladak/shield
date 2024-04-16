b5521852b2a2fffe86a3f8b02afe7ecc694b7032
Vladimir Kotal <vlada@kotalovi.cz>
2024-04-16 23:39:45 +0200

    add missing parens

--- safemode.py
+++ safemode.py
@@ -18,7 +18,7 @@ if reason == supervisor.SafeModeReason.HARD_FAULT:
     microcontroller.reset()  # Reset and start over.
 elif reason == supervisor.SafeModeReason.BROWNOUT:
     # Sleep for ten minutes and then run code.py again.
-    time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic + 10 * 60)
+    time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 10 * 60)
     alarm.exit_and_deep_sleep_until_alarms(time_alarm)
 elif reason == supervisor.SafeModeReason.SAFE_MODE_WATCHDOG:
     # pylint: disable=no-member

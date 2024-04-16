76a2bdb155a0c83868af02d5032c49f3377999f5
Vladimir Kotal <vlada@kotalovi.cz>
2024-04-16 21:59:41 +0200

    handle pylint

--- safemode.py
+++ safemode.py
@@ -4,19 +4,23 @@ handle selected modes of safe mode
 
 import time
 
+# pylint: disable=import-error
 import alarm
 import microcontroller
+# pylint: disable=import-error
 import supervisor
 
 safe_mode_reason = supervisor.runtime.safe_mode_reason
 
 if safe_mode_reason == supervisor.SafeModeReason.HARD_FAULT:
+    # pylint: disable=no-member
     microcontroller.reset()  # Reset and start over.
 elif safe_mode_reason == supervisor.SafeModeReason.BROWNOUT:
     # Sleep for ten minutes and then run code.py again.
     time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic + 10 * 60)
     alarm.exit_and_deep_sleep_until_alarms(time_alarm)
 elif safe_mode_reason == supervisor.SafeModeReason.SAFE_MODE_WATCHDOG:
+    # pylint: disable=no-member
     microcontroller.reset()  # Reset and start over.
 
 # Otherwise, do nothing. The safe mode reason will be printed in the

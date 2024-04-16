cc91a5f7778ced9fae0fe32081e19812642ed303
Vladimir Kotal <vlada@kotalovi.cz>
2024-04-16 23:34:27 +0200

    fix error: unsupported types for __add__: function, int

--- safemode.py
+++ safemode.py
@@ -11,16 +11,16 @@ import microcontroller
 # pylint: disable=import-error
 import supervisor
 
-safe_mode_reason = supervisor.runtime.safe_mode_reason
+reason = supervisor.runtime.safe_mode_reason
 
-if safe_mode_reason == supervisor.SafeModeReason.HARD_FAULT:
+if reason == supervisor.SafeModeReason.HARD_FAULT:
     # pylint: disable=no-member
     microcontroller.reset()  # Reset and start over.
-elif safe_mode_reason == supervisor.SafeModeReason.BROWNOUT:
+elif reason == supervisor.SafeModeReason.BROWNOUT:
     # Sleep for ten minutes and then run code.py again.
     time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic + 10 * 60)
     alarm.exit_and_deep_sleep_until_alarms(time_alarm)
-elif safe_mode_reason == supervisor.SafeModeReason.SAFE_MODE_WATCHDOG:
+elif reason == supervisor.SafeModeReason.SAFE_MODE_WATCHDOG:
     # pylint: disable=no-member
     microcontroller.reset()  # Reset and start over.
 

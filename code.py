5c6f72aba3874a9d9f92bb8915b97a165259a52b
Vladimir Kotal <vlada@kotalovi.cz>
2024-05-09 22:24:37 +0200

    refactor

--- code.py
+++ code.py
@@ -212,55 +212,7 @@ def main():
 
     sensors = Sensors(i2c)
 
-    mqtt_client = None
-    rfm69 = None
-    # Try packetized radio first. If that does not work, fall back to WiFi.
-    try:
-        spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-        # The D-pin values assume certain wiring of the Radio FeatherWing.
-        cs = digitalio.DigitalInOut(board.D14)
-        reset = digitalio.DigitalInOut(board.D32)
-        rfm69 = adafruit_rfm69.RFM69(
-            spi, cs, reset, 433
-        )  # hard-coded frequency for Europe
-
-        tx_power = secrets.get(TX_POWER)
-        if rfm69.high_power and tx_power:
-            logger.debug(f"setting TX power to {tx_power}")
-            rfm69.tx_power = tx_power
-
-        encryption_key = secrets.get(ENCRYPTION_KEY)
-        if encryption_key:
-            logger.debug("Setting encryption key")
-            rfm69.encryption_key = encryption_key
-    except Exception:  # pylint: disable=broad-exception-caught
-        logger.debug(f"MAC address: {wifi.radio.mac_address}")
-
-        # Connect to Wi-Fi
-        logger.info("Connecting to wifi")
-        wifi.radio.connect(secrets[SSID], secrets[PASSWORD], timeout=10)
-        logger.info(f"Connected to {secrets['ssid']}")
-        logger.debug(f"IP: {wifi.radio.ipv4_address}")
-
-        # Create a socket pool
-        pool = socketpool.SocketPool(wifi.radio)  # pylint: disable=no-member
-
-        broker_addr = secrets[BROKER]
-        broker_port = secrets[BROKER_PORT]
-        mqtt_client = mqtt_client_setup(pool, broker_addr, broker_port, log_level)
-        try:
-            log_topic = secrets[LOG_TOPIC]
-            # Log both to the console and via MQTT messages.
-            # Up to now the logger was using the default (built-in) handler,
-            # now it is necessary to add the Stream handler explicitly as
-            # with a non-default handler set only the non-default handlers will be used.
-            logger.addHandler(logging.StreamHandler())
-            logger.addHandler(MQTTHandler(mqtt_client, log_topic))
-        except KeyError:
-            pass
-
-        logger.info(f"Attempting to connect to MQTT broker {broker_addr}:{broker_port}")
-        mqtt_client.connect()
+    mqtt_client, rfm69 = setup_transport()
 
     # MQTT topic is used for both transports.
     mqtt_topic = secrets[MQTT_TOPIC]
@@ -346,6 +298,67 @@ def main():
     sleep_duration = get_sleep_duration(battery_monitor, logger)
 
     enter_sleep(sleep_duration, SleepKind(SleepKind.DEEP))
+
+
+def setup_transport():
+    """
+    Setup transport to send data.
+    Return a tuple of RFM69 object and MQTT client object, either can be None.
+    """
+    logger = logging.getLogger(__name__)
+
+    mqtt_client = None
+    rfm69 = None
+    # Try packetized radio first. If that does not work, fall back to WiFi.
+    try:
+        spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+        # The D-pin values assume certain wiring of the Radio FeatherWing.
+        cs = digitalio.DigitalInOut(board.D14)
+        reset = digitalio.DigitalInOut(board.D32)
+        logger.info("Setting up RFM69")
+        rfm69 = adafruit_rfm69.RFM69(
+            spi, cs, reset, 433
+        )  # hard-coded frequency for Europe
+
+        tx_power = secrets.get(TX_POWER)
+        if rfm69.high_power and tx_power:
+            logger.debug(f"setting TX power to {tx_power}")
+            rfm69.tx_power = tx_power
+
+        encryption_key = secrets.get(ENCRYPTION_KEY)
+        if encryption_key:
+            logger.debug("Setting encryption key")
+            rfm69.encryption_key = encryption_key
+    except Exception:  # pylint: disable=broad-exception-caught
+        logger.debug(f"MAC address: {wifi.radio.mac_address}")
+
+        # Connect to Wi-Fi
+        logger.info("Connecting to wifi")
+        wifi.radio.connect(secrets[SSID], secrets[PASSWORD], timeout=10)
+        logger.info(f"Connected to {secrets['ssid']}")
+        logger.debug(f"IP: {wifi.radio.ipv4_address}")
+
+        # Create a socket pool
+        pool = socketpool.SocketPool(wifi.radio)  # pylint: disable=no-member
+
+        broker_addr = secrets[BROKER]
+        broker_port = secrets[BROKER_PORT]
+        mqtt_client = mqtt_client_setup(pool, broker_addr, broker_port, logger.getEffectiveLevel())
+        try:
+            log_topic = secrets[LOG_TOPIC]
+            # Log both to the console and via MQTT messages.
+            # Up to now the logger was using the default (built-in) handler,
+            # now it is necessary to add the Stream handler explicitly as
+            # with a non-default handler set only the non-default handlers will be used.
+            logger.addHandler(logging.StreamHandler())
+            logger.addHandler(MQTTHandler(mqtt_client, log_topic))
+        except KeyError:
+            pass
+
+        logger.info(f"Attempting to connect to MQTT broker {broker_addr}:{broker_port}")
+        mqtt_client.connect()
+
+    return mqtt_client, rfm69
 
 
 def get_sleep_duration(battery_monitor, logger):

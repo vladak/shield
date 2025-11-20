1331200bfa6dea7e50df94bc4b8c841cb2622272
Vladimir Kotal <vlada@kotalovi.cz>
2025-11-20 19:25:51 +0100

    add more coverage
    for the case of running without battery monitor

--- test_sleep.py
+++ test_sleep.py
@@ -11,23 +11,31 @@ from names import *
 from sleep import get_deep_sleep_duration
 
 
-@pytest.mark.parametrize("above", [True, False])
-def test_get_deep_sleep_duration(above: bool):
+@pytest.mark.parametrize(
+    "above,is_battery", [(True, True), (True, False), (False, True), (False, False)]
+)
+def test_get_deep_sleep_duration(above: bool, is_battery: bool):
     """
     Test the get_deep_sleep_duration function to return short/long sleep duration
     for battery capacity above/below threshold, respectively.
+    If there is no battery monitor, the sleep duration should be always
+    of the long sleep duration.
     """
     secrets = {
         DEEP_SLEEP_DURATION: 42,
         SLEEP_DURATION_SHORT: 10,
         BATTERY_CAPACITY_THRESHOLD: 50,
     }
-    battery_monitor = Mock()
-    if above:
-        battery_monitor.cell_percent = secrets[BATTERY_CAPACITY_THRESHOLD] + 1
-        expected_duration = secrets[SLEEP_DURATION_SHORT]
+    battery_monitor = None
+    if is_battery:
+        battery_monitor = Mock()
+        if above:
+            battery_monitor.cell_percent = secrets[BATTERY_CAPACITY_THRESHOLD] + 1
+            expected_duration = secrets[SLEEP_DURATION_SHORT]
+        else:
+            battery_monitor.cell_percent = secrets[BATTERY_CAPACITY_THRESHOLD]
+            expected_duration = secrets[DEEP_SLEEP_DURATION]
     else:
-        battery_monitor.cell_percent = secrets[BATTERY_CAPACITY_THRESHOLD]
         expected_duration = secrets[DEEP_SLEEP_DURATION]
 
     logger = Mock()
